87bf36dab7855d0b1b4287c8e51a3663135a268f
Paula Lopez Jimenez <paula.lj17@gmail.com>
2020-01-18 23:14:29 +0100

    ya casi funsiona

---
+++ buscarJS.js
@@ -0,0 +1,122 @@
+var url = "http://www.omdbapi.com/?"
+var apikey = "apikey=56686ffa"
+var pagina = 1;
+$("#buscar").on("click", () => reiniciar());
+$(window).scroll(buscar);
+
+function reiniciar() {
+    $('#div1').empty();
+    pagina = 1;
+    buscar();
+}
+
+function buscar() {
+    if (($(window).scrollTop() + $(window).height() >= $(document).height() - 100)) {
+        var cargar = $('<div>');
+        $(cargar).attr('class', 'spinner-grow text-primary');
+        $('#div1').append(cargar);
+        var nombreBuscar = $("#titulo").val();
+        var nombre = nombreBuscar.split(" ");
+        var buscar = "";
+        for (x = 0; x < nombre.length; x++) {
+            if (x == 0 && nombre.length > 1) {
+                buscar += nombre[x] + "+";
+            } else if (x < nombre.length - 1) {
+                buscar += nombre[x] + "+";
+            } else {
+                buscar += nombre[x];
+            }
+        }
+        var urlBuscar = url + "s=" + buscar + "&page=" + pagina + "&" + apikey
+        console.log(urlBuscar);
+        $.ajax({
+            url: urlBuscar,
+            success: function (respuesta) {
+                console.log(respuesta);
+                colocarPeliculas(respuesta);
+                $(cargar).remove();
+            }
+        })
+        pagina++;
+    }
+}
+
+function colocarPeliculas(respuesta) {
+    $('#div1').attr('class', 'row')
+    $.each(respuesta.Search, function (ind, peliculas) {
+        //
+        let pelicula = $('<div>');
+        $(pelicula).attr('class', 'card text-center');
+        $(pelicula).on('click', () => datosPelicula(pelicula.id))
+        pelicula.id = peliculas.imdbID;
+        $('#div1').append(pelicula);
+
+        //
+        let imagen = $('<img>');
+        imagen.url = peliculas.Poster;
+        $(imagen).attr('class', 'card-img-top');
+        if (imagen.url == 'N/A') {
+            $(imagen).attr('src', './imagen/noImagen.jpg');
+        } else {
+            $(imagen).attr('src', imagen.url);
+        }
+        $(pelicula).append(imagen);
+        $(pelicula).mouseenter(function () {
+            $(imagen).css({
+                "opacity": "0.6 ",
+                "transition": "2s"
+            })
+            $(pelicula).css({
+                "border": " 3px inset blue"
+            })
+        })
+        $(pelicula).mouseleave(function () {
+            $(imagen).css({
+                "opacity": "1",
+                "transition": "1s"
+            })
+            $(pelicula).css({
+                "border": "1px solid black"
+            })
+        })
+        //
+        let cuerpo = $('<div>');
+        $(cuerpo).attr('class', 'card-body ');
+        let titulo = $('<h5>');
+        $(titulo).attr('class', 'card-title');
+        $(titulo).text(peliculas.Title);
+        $(pelicula).append(cuerpo);
+        $(cuerpo).append(titulo);
+    })
+
+}
+
+function datosPelicula(id) {
+    let id1 = "i=" + id;
+    let urlDatos = url + id1 + "&" + apikey;
+    $.ajax({
+        url: urlDatos,
+        success: function (respuesta) {
+            console.log(respuesta);
+            colocarModal(respuesta);
+        }
+    })
+
+}
+
+function colocarModal(respuesta) {
+    $('.modal-title').text(respuesta.Title);
+    if (respuesta.Poster == 'N/A') {
+        $('#imagen').attr('src', './imagen/noImagen.jpg');
+    } else {
+        $('#imagen').attr('src', respuesta.Poster);
+    }
+    $('#genero').text(respuesta.Genre)
+    $('#estreno').text(respuesta.Released)
+    $('#director').text(respuesta.Director)
+    $('#escritor').text(respuesta.Writer)
+    $('#actores').text(respuesta.Actors)
+    $('#sinopsis').text(respuesta.Plot)
+    $('#puntuacion').text(respuesta.imdbRating)
+    $('#modelId').modal("show");
+}
