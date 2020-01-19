2267c1bb895a1718c8ad1c5258120ec13cf6544d
Paula Lopez Jimenez <paula.lj17@gmail.com>
2020-01-19 22:26:23 +0100

    ya esta comentado

--- buscarJS.js
+++ buscarJS.js
@@ -3,22 +3,22 @@ var apikey = "apikey=56686ffa"
 var pagina = 1;
 $("#buscar").on("click", () => reiniciar());
 $(window).scroll(buscar);
-//
+// vacia el div, reinicia la variable de las paginas a 1 y realiza la funcion buscar
 function reiniciar() {
     $('#div1').empty();
     pagina = 1;
     buscar();
 }
-//
+// comprueba la posicion del scroll y mete el spinner de carga, coge el nombre del input y crea la url completa y busca la respuesta
 function buscar() {
-    if (($(window).scrollTop() + $(window).height() >= $(document).height() - 150)) {
+    if (($(window).scrollTop()+ $(window).height()  >= $(document).height() - 150)) {
         var cargar = $('<div>');
         $(cargar).attr('class', 'spinner-grow text-primary');
         $('#div1').append(cargar);
         var nombreBuscar = $("#titulo").val();
         var nombre = nombreBuscar.split(" ");
         var buscar = "";
-        //
+        //en caso de que el nombre contenga mas de una palabra las une por los espacion con +
         for (x = 0; x < nombre.length; x++) {
             if (x == 0 && nombre.length > 1) {
                 buscar += nombre[x] + "+";
@@ -31,6 +31,7 @@ function buscar() {
         var urlBuscar = url + "s=" + buscar + "&page=" + pagina + "&" + apikey
         console.log(urlBuscar);
         $.ajax({
+            //coge la url, coloca la respuestas y borra el spinner de carga y suma uno a la pagina 
             url: urlBuscar,
             success: function (respuesta) {
                 console.log(respuesta);
@@ -41,18 +42,19 @@ function buscar() {
         pagina++;
     }
 }
-
+//pasandole como parametro el array de peliculas coloca las peliculas
 function colocarPeliculas(respuesta) {
     $('#div1').attr('class', 'row justify-content-center')
     $.each(respuesta.Search, function (ind, peliculas) {
-        //
+        //por cada respuesta encontrada crea un div card y al hacer click en el div hace la funcion datosPelicula()
         let pelicula = $('<div>');
         $(pelicula).attr('class', 'card text-center');
         $(pelicula).on('click', () => datosPelicula(pelicula.id))
         pelicula.id = peliculas.imdbID;
         $('#div1').append(pelicula);
 
-        //
+        //a cada div card le inserta un img con la url que que encuentra en la respuesta de cada pelicula, 
+        //en caso de que la respuesta no tenga imagen mete una imagen por defecto
         let imagen = $('<img>');
         imagen.url = peliculas.Poster;
         $(imagen).attr('class', 'card-img-top');
@@ -61,8 +63,8 @@ function colocarPeliculas(respuesta) {
         } else {
             $(imagen).attr('src', imagen.url);
         }
-        //
         $(pelicula).append(imagen);
+        //transicion al pasar el raton por encima del los div card
         $(pelicula).mouseenter(function () {
             $(imagen).css({
                 "opacity": "0.6 ",
@@ -86,7 +88,7 @@ function colocarPeliculas(respuesta) {
                 "transition": "1s"
             })
         })
-        //
+        //Añade al div card un div con un h5 que contiene el titulo de la carta
         let cuerpo = $('<div>');
         $(cuerpo).attr('class', 'card-body ');
         let titulo = $('<h5>');
@@ -97,7 +99,7 @@ function colocarPeliculas(respuesta) {
     })
 
 }
-//
+//pasandole como parametro el id de la pelicula, busca todos los datos sobre la pelicula
 function datosPelicula(id) {
     let id1 = "i=" + id;
     let urlDatos = url + id1 + "&" + apikey;
@@ -110,7 +112,8 @@ function datosPelicula(id) {
     })
 
 }
-//
+//pasandole como parametro los datos de la pelicula, coge la valoracion y rellena las estrellas, 
+// la clase checked esta definida en el css
 function pintarEstrellas(respuesta) {
     let puntuacion = respuesta.imdbRating;
     switch (Math.round((puntuacion / 2))) {
@@ -157,9 +160,10 @@ function pintarEstrellas(respuesta) {
             $('#5').attr('class', 'fa fa-star-o');
     }
 }
-//
+//pasandole como parametro los datos de la pelicula rellena el modal con los datos recibidos
 function colocarModal(respuesta) {
     $('.modal-title').text(respuesta.Title);
+    //en caso de que la pelicula no tenga imagen le pone una imagen por defecto
     if (respuesta.Poster == 'N/A') {
         $('#imagen').attr('src', './imagen/noImagen.jpg');
     } else {
